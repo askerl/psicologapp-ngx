63ad9760da833899c520a79de719347d2f012859
Alfredo Skerl <Alfredo.X.Skerl.-nd@disney.com>
2018-01-23 18:58:14 -0300

    - Fix lint errors

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,73 +1,76 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import 'rxjs/add/observable/of';
 
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Injectable()
 export class DataService {
 
-	private pacientesCollection: AngularFirestoreCollection<any> = this.afs.collection('pacientes', ref => ref.orderBy("apellido","asc").orderBy("nombre","asc"));
+  private pacientesCollection: AngularFirestoreCollection<any> = this.afs.collection('pacientes',
+    ref => ref.orderBy('apellido', 'asc').orderBy('nombre', 'asc'));
   pacientes: Observable<any[]>;
-  
+
   tipoPaciente = {
-    "O": { nombre: "Obra social", color: "primary" },
-    "P": { nombre: "Privado", color: "warning" },
+    'O': { nombre: 'Obra social', color: 'primary' },
+    'P': { nombre: 'Privado', color: 'warning' },
   };
 
   prepagasById = {
-    "galeno": {
-        nombre: "Galeno",
+    'galeno': {
+        nombre: 'Galeno',
         pagos: [15, 175, 452.36],
     },
-    "ososs": {
-        nombre: "OSOSS",
+    'ososs': {
+        nombre: 'OSOSS',
         pagos: [230],
     },
-    "ospacp": {
-        nombre: "O.S.P.A.C.P",
+    'ospacp': {
+        nombre: 'O.S.P.A.C.P',
         pagos: [230],
-    }
-}
+    },
+  }
 
-	constructor(private readonly afs: AngularFirestore) {
+  constructor(private readonly afs: AngularFirestore) {
 
   }
-  
+
   getPacientes() {
     this.pacientes = this.pacientesCollection.snapshotChanges().map(actions => {
-			return actions.map(a => {
-				const id = a.payload.doc.id;
-			  let paciente = a.payload.doc.data();
+      return actions.map(a => {
+        const id = a.payload.doc.id;
+        const paciente = a.payload.doc.data();
         paciente.nombreCompleto = `${paciente.apellido}, ${paciente.nombre}`;
         paciente.sesionesRestantes = paciente.sesionesAut ? (paciente.sesionesAut - paciente.sesiones) : '';
-        let porcs = this.calcPorcentajesSesiones(paciente.sesionesAut, paciente.sesiones);
+        const porcs = this.calcPorcentajesSesiones(paciente.sesionesAut, paciente.sesiones);
         paciente.porcRestantes = porcs.porcRestantes;
-			  return { id, ...paciente };
-			});
-		});
+        return { id, ...paciente };
+      });
+    });
   }
 
   calcPorcentajesSesiones(sesionesAut, sesiones) {
-    let porcUsadas = sesionesAut > 0 ? sesiones / sesionesAut * 100 : 0;
-    let porcRestantes = sesionesAut > 0 ? (sesionesAut - sesiones)/ sesionesAut * 100 : 0 ;
+    const porcUsadas = sesionesAut > 0 ? sesiones / sesionesAut * 100 : 0;
+    const porcRestantes = sesionesAut > 0 ? (sesionesAut - sesiones) / sesionesAut * 100 : 0 ;
     return {porcUsadas, porcRestantes};
   }
 
   getFiltroTipoPaciente() {
-    let list = [];
-    for (const key in this.tipoPaciente) {
-      list.push({ value: key, title: this.tipoPaciente[key].nombre })
+    const list = [], obj = this.tipoPaciente;
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        list.push({ value: key, title: obj[key].nombre })
+      }
     }
     return list;
   }
 
   getFiltroPrepaga() {
-    let list = [];
-    for (const key in this.prepagasById) {
-      list.push({ value: key, title: this.prepagasById[key].nombre })
+    const list = [], obj = this.prepagasById;
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        list.push({ value: key, title: obj[key].nombre })
+      }
     }
     return list;
   }
